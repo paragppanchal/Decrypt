219916e3af8de4fc70d5e58e567142d42fcee8f9
edward ottorino <edward.ottorino@gmail.com>
2018-08-23 14:34:44 +1000

    email works for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating users"
+User.destroy_all
+
+user = User.new(email: 'loviyakane@poly-swarm.com', password: 'password')
+user.save!
+
+user_two = User.new(email: 'emaill@gmail.com', password: 'password')
+user_two.save!
+
+user_three = User.new(email: 'emailll@gmail.com', password: 'password')
+user_three.save!
+
+puts "done"
