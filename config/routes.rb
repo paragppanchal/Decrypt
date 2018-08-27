260299b20e1abcd954f6d639ffbd0b8c6253c110
testname <testemail>
2018-08-27 15:49:09 +1000

    learning portal done :D

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,14 @@ Rails.application.routes.draw do
   get 'sell/bitcoin', to: 'sell_rate#bitcoin', as: :sell_bitcoin
   get 'learn', to: 'learning_portal#home', as: :learn
   get 'learn/bitcoin', to: 'learning_portal#bitcoin', as: :learn_bitcoin
+  get 'learn/bitcoin/:id', to: 'learning_portal#show', as: :learn_bitcoin_show
+  get 'learn/introduction-to-cryptocurrency', to: 'learning_portal#introduction'
+  get 'learn/cryptocurrency-origin', to: 'learning_portal#origin'
+  get 'learn/cryptocurrency-pros-cons', to: 'learning_portal#proscons'
+  get 'learn/cryptocurrency-types', to: 'learning_portal#types'
+  get 'learn/cryptocurrency-risks-dangers', to: 'learning_portal#risks-dangers'
+
+
   root to: 'pages#home', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
