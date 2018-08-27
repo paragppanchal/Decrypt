a449fdb247a87b58535a928cd87bea88ab70ee4f
Parag Panchal <parag@relteksys.com.au>
2018-08-27 18:19:46 +1000

    search and filter methods are added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'exchanges/show'
   get 'buy/bitcoin', to: 'buy_rate#bitcoin', as: :buy_bitcoin
   get 'sell/bitcoin', to: 'sell_rate#bitcoin', as: :sell_bitcoin
+  # get 'buy/:coin', to: "buy_rate#buy", as: :buy_coin
+
   get 'learn', to: 'learning_portal#home', as: :learn
   get 'learn/bitcoin', to: 'learning_portal#bitcoin', as: :learn_bitcoin
   root to: 'pages#home', as: :home
